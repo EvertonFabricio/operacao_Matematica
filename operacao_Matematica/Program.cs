c7ab246c0f07e0cb6535b7fe9b7ca2c5a4df53e4
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 09:53:57 -0300

    Adicionar arquivos de projeto.

---
+++ operacao_Matematica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace operacao_Matematica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro:");
+			int n1 = (int.Parse(Console.ReadLine()));
+
+			Console.WriteLine("\nDigite outro numero inteiro:");
+			int n2 = (int.Parse(Console.ReadLine()));
+
+			Console.WriteLine("\nEscolha uma operação matemática para ser feita.\nDigite + para somar, - para subtrair, * para multiplicar ou / para dividir!");						
+			var operador = (char.Parse(Console.ReadLine()));
+         
+			if (operador == '+')
+				Console.WriteLine($"\nA soma dos numeros é {n1 + n2}");
+			else if (operador == '-')
+				Console.WriteLine($"\nA subtração dos numeros é {n1 - n2}");
+			else if (operador == '*')
+				Console.WriteLine($"\nA multiplicação dos numeros é {n1 * n2}");
+			else if (operador == '/' & n2 != 0)
+				Console.WriteLine($"\nA divisão dos numeros é {n1 + n2}");
+			else 
+				Console.WriteLine($"\nimpossivel dividir por zero!");
+
+		}
+    }
+}
+
+
+
+
